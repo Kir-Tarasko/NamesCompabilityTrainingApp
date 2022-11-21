c8ebe80df70f3c194bf1d8197d50149422ce0177
KirTarasko <kir.tarasko@gmail.com>
2022-11-21 10:15:19 +0200

    Initial Commit

---
+++ NamesCompabilityTrainingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesCompabilityTrainingApp
+//
+//  Created by Kirill Tarasko on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
